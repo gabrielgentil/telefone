4b49a7b700ab7584df2f236520c5a2607c97989f
Gabriel Santos Gentil <gabrielgentil@Gabriels-MacBook-Air.local>
2020-01-16 00:28:43 -0300

    aplicacao ligacao telefonica

---
+++ monitor/server.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ path: __dirname + '/../.env' })
+const axios = require("axios");
+const TotalVoice = require("totalvoice-node");
+const client = new TotalVoice(process.env.TOTALVOICE_API_KEY);
+
+const servers = [
+  {
+    name: 'Servidor 1',
+    url: 'http://localhost:4001',
+    developer: {
+      name: process.env.NOME,
+      telephone: process.env.NUMERO_TELEFONE
+    }
+  },
+  {
+    name: 'Servidor 2',
+    url: 'http://localhost:4002',
+    developer: {
+      name: process.env.NOME,
+      telephone: process.env.NUMERO_TELEFONE
+    }
+  }]
+
+async function telefone() {
+	console.log("Iniciando monitoramento dos servidores");
+	for (const server of servers) {
+		await axios({
+			url: server.url,
+			method: "get"
+		}).then((response) => {
+			console.log(`${server.name} está no ar!`);
+		}).catch(() => {
+			console.log(`${server.name} está fora do ar!`);
+			const message = `${server.developer.name} o ${server.name} está fora do ar, por favor faça alguma coisa o mais rápido possível!`;  
+			const options = {
+				velocidade: 2,
+				tipo_voz: "br-Vitoria"
+			};
+			client.tts.enviar(server.developer.telephone, message, options).then(() => {
+				console.log(`O desenvolvedor ${server.developer.name} já foi avisado!`);
+			});
+		});
+	}
+	console.log("Finalizando monitoramento dos servidores");
+};
+
+telefone();
